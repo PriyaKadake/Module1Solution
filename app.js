97763d59f0aae12e097d15c537443f7c02419078
priya <jpriya162@gmail.com>
2017-03-18 18:10:05 +0530

    updated for colors

--- app.js
+++ app.js
@@ -17,13 +17,20 @@ function LunchCheckController ($scope) {
 
   function checkMenu(menu) {
     if(menu.length==0){
+      document.getElementById('menuMsg').style.color="red";
+      document.getElementById('lunch-menu').style.borderColor="red";
       return "Please enter data first";
     }else{
       var menuArr=menu.split(",");
       var menuArrLen=menuArr.length;
+
       if(menuArrLen<=3){
+        document.getElementById('menuMsg').style.color="green";
+        document.getElementById('lunch-menu').style.borderColor="green";
         return "Enjoy!";
       }else{
+        document.getElementById('menuMsg').style.color="green";
+        document.getElementById('lunch-menu').style.borderColor="green";
         return "Too much!";
       }
     }
